f010f6e9f8c489c566f726dd014673799039640b
Ubuntu <ubuntu@ip-172-31-30-116.eu-north-1.compute.internal>
2025-12-10 10:33:54 +0000

    fix: fixd chore issue in backend

--- mysite/storage_backends.py
+++ mysite/storage_backends.py
@@ -1,11 +1,19 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public-read'
-
+    location = "static"
+    default_acl = None
+    file_overwrite = True
+    custom_domain = False
+    object_parameters = {
+        "CacheControl": "max-age=31536000, public",  # Cache for 1 year
+    }
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = "media"
+    default_acl = None
     file_overwrite = False
-    default_acl = 'public-read'
+    custom_domain = False
+    object_parameters = {
+        "CacheControl": "max-age=86400, public",  # Cache for 1 day
+    }
